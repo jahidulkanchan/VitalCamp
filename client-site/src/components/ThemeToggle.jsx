e61a7e3762419c7dba879f0e01feabfb684666a9
jahidulkanchan <mdj524156@gmail.com>
2025-05-03 15:55:36 +0600

    upload file

---
+++ client-site/src/components/ThemeToggle.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { IoMdSunny } from 'react-icons/io';
+import { MdDarkMode } from 'react-icons/md';
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+      document.documentElement.classList.remove('light');
+    } else {
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('light');
+    }
+
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  return (
+    <button onClick={toggleTheme} className="p-2 rounded-full bg-white dark:bg-color-dark-lite">
+      {theme === 'dark' ? <IoMdSunny size={20} color="#000000" /> : <MdDarkMode size={20} color="#1E1E1E" />}
+    </button>
+  );
+};
+
+export default ThemeToggle;
